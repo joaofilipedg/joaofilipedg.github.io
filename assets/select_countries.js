87c72621733d8f8337fcbe9ef160da893eccb26c
João Guerreiro (via Travis CI) <joaofilipedg@gmail.com>
2020-05-13 16:38:16 +0000

    Commit development@c95827d85c328f10a86d158b68538ec494653b20 pushed to GitHub-Pages by Travis CI Build 21

--- assets/select_countries.js
+++ assets/select_countries.js
@@ -57,11 +57,23 @@ function autocomplete(inp, arr) {
                 b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
                 /*execute a function when someone clicks on the item value (DIV element):*/
                 b.addEventListener("click", function (e) {
+                    selected_c = this.getElementsByTagName("input")[0].value;
                     /*insert the value for the autocomplete text field:*/
-                    inp.value = this.getElementsByTagName("input")[0].value;
+                    inp.value = selected_c;
+                    
                     /*close the list of autocompleted values,
                     (or any other open lists of autocompleted values:*/
                     closeAllLists();
+                    
+                    if (list_active_countries.includes(selected_c)) {
+                        alert("Country already selected");
+                        inp.value = "";
+                    } else {
+                        // document.getElementById("fast_but").style.visibility = "hidden";
+                        console.log(`Country ${selected_c} validated`)
+                        getNewCountryData(selected_c);
+                        addNewCountry(selected_c);
+                    }
                 });
                 a.appendChild(b);
             }
